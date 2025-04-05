7d531448d3adcab70970ab93d17847c4199c7c70
ravish <47687766+webRavish456@users.noreply.github.com>
2025-04-05 12:26:44 +0530

    commit branch

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -50,3 +50,51 @@ export const postAdmin = async (req, res) => {
 
   } 
 };
+
+export const postForgot = async (req, res) => {
+
+  const ContentType = req.headers["content-type"];
+
+  if (ContentType && ContentType.includes("multipart/form-data")) {
+
+    upload.none()(req, res, async (err) => {
+      if (err) {
+        return res.status(500).json({ status: "error", msg: "Error handling form data" });
+      }
+
+  try {
+
+    const { email, password, confirmpassword } = req.body;
+
+    if (!email || !password || !confirmpassword) {
+      return res.status(400).json({ status: "error", message: "All fields are required" });
+    }
+
+    if (password !== confirmpassword) {
+      return res.status(400).json({ status: "error", message: "Passwords do not match" });
+    }
+
+    const existingAdmin = await AdminModel.findOne({ email });
+
+    if (!existingAdmin) {
+      return res.status(404).json({ status: "error", message: "Admin not found" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await AdminModel.updateOne(
+      { email },
+      { $set: { password: hashedPassword } }
+    );
+
+    res.status(200).json({ status: "success", message: "Password updated successfully!" });
+
+  } catch (error) {
+    console.error("Error updating password:", error);
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+  }
+  
+)}
+
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,19 @@
 import express from 'express'
-import { postAdmin } from '../controllers/authControllers.js';
+import { postAdmin, postForgot } from '../controllers/authControllers.js';
+import { deleteBranch, getBranch, getBranchById, postBranch, updateBranch } from '../controllers/branchControllers.js';
 
 
 export const router = express.Router();
 
+/* auth API endpoints */
+
 router.route('/login').post(postAdmin);
+router.route('/forgot-password').post(postForgot);
+
+/* branch */
+
+router.route('/branch').post(postBranch)
+router.route('/branch').get(getBranch)
+router.route('/branch/:id').get(getBranchById)
+router.route('/branch/:id').patch(updateBranch)
+router.route('/branch/:id').delete(deleteBranch)
