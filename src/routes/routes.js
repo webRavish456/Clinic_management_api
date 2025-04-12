eb8952a1a9b0e92fda473f1b670b7e0ec20615cd
ravish <47687766+webRavish456@users.noreply.github.com>
2025-04-12 08:21:08 +0530

    commit add token

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import { postAdmin, postForgot } from '../controllers/authControllers.js';
 import { deleteBranch, getBranch, getBranchById, postBranch, updateBranch } from '../controllers/branchControllers.js';
 import { deleteAllPatients, getAllPatients, getAllPatientsById, postAllPatients, updateAllPatients } from '../controllers/allpatientsControllers.js';
 import uploadPatient from '../upload/patient.js';
+import verifyToken from '../middleware/auth.js';
 
 
 export const router = express.Router();
@@ -14,16 +15,16 @@ router.route('/forgot-password').post(postForgot);
 
 /* branch */
 
-router.route('/branch').post(postBranch)
-router.route('/branch').get(getBranch)
-router.route('/branch/:id').get(getBranchById)
-router.route('/branch/:id').patch(updateBranch)
-router.route('/branch/:id').delete(deleteBranch)
+router.route('/branch').post(verifyToken, postBranch)
+router.route('/branch').get(verifyToken, getBranch)
+router.route('/branch/:id').get(verifyToken, getBranchById)
+router.route('/branch/:id').patch(verifyToken, updateBranch)
+router.route('/branch/:id').delete(verifyToken, deleteBranch)
 
 
 /* patients */
-router.route('/allpatients').post(uploadPatient, postAllPatients)
-router.route('/allpatients').get(getAllPatients)
-router.route('/allpatients/:id').get(getAllPatientsById)
-router.route('/allpatients/:id').patch(uploadPatient, updateAllPatients)
-router.route('/allpatients/:id').delete(deleteAllPatients)
+router.route('/allpatients').post(verifyToken, uploadPatient, postAllPatients)
+router.route('/allpatients').get(verifyToken, getAllPatients)
+router.route('/allpatients/:id').get(verifyToken, getAllPatientsById)
+router.route('/allpatients/:id').patch(verifyToken, uploadPatient, updateAllPatients)
+router.route('/allpatients/:id').delete(verifyToken, deleteAllPatients)
