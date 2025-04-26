408391a6cc39e121788ecf9cf05a59d4fa3e1997
SnehanjaliBiswal <biswalsnehanjali77@gmail.com>
2025-04-26 12:28:44 +0530

    commit changes

--- src/controllers/patientsrecordsControllers.js
+++ src/controllers/patientsrecordsControllers.js
@@ -16,20 +16,24 @@ export const postPatientsRecords = async (req, res) => {
       if (! patientName || !doctorNotes || !nextFollowUp || !mobileNo  || !req.imageUrls?.image) {
         return res.status(400).json({ status: "error", message: "All fields are required" });
       }
+
+      console.log(req.body)
       const labReport = req.imageUrls?.image;
 
       const patient= await AllPatientsModel.findOne({mobileNo})
 
-      if (patient.name !== patientName) {
-        return res.status(400).json({ status: "error", message: "Patient not found" });
-      }
-
-      const appointment= await AppointmentModel.findOne({mobileNo})
-
-      if (appointment.patientName !== patientName) {
-        return res.status(400).json({ status: "error", message: "Please make an appointment first" });
-      }
+      
+          if (patient?.name !== patientName) {
+           return res.status(400).json({ status: "error", message: "Patient not found" });
+        }
+    
+      const appointment= await AppointmentModel.findOne({mobileNo:mobileNo})
+     
    
+        if (appointment?.patientName !== patientName) {
+          return res.status(400).json({ status: "error", message: "Please make an appointment first" });
+        }
+     
       const newPatientsRecords = await PatientsRecordsModel.create({ patientName, doctorNotes,  nextFollowUp, labReport, patient, mobileNo, treatment:appointment.treatment, doctorAssigned: appointment.doctorName });
 
       res.status(200).json({ status: "success", message: " Patients Records created successfully!" });
